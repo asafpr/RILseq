810d9dbcf2e51f365e486068240280ec7202cb42
Asaf Peer <asafpr@gmail.com>
2015-12-28 05:36:15 +0200

    fixed sum_reads

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
     
 setup(name='RILseq',
-      version='0.38',
+      version='0.39',
       description='Processing RILSeq experiments results',
       long_description=readme(),
       classifiers=[
