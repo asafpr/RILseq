d541d6f07d2d4c4e1b1a6de71b85863f44d02a0a
Niv Reiss <niv@lazuli.hanah>
2017-09-19 11:56:21 +0300

    changed ecocyc logo to biocyc logo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
     
 setup(name='RILseq',
-      version='0.51',
+      version='0.52',
       description='Processing RILSeq experiments results',
       long_description=readme(),
       classifiers=[
