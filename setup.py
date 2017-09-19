01152c79bf2947ced8b3d938460db05d5a780093
Niv Reiss <niv@lazuli.hanah>
2017-09-19 11:23:37 +0300

    fixed genome 3 filenames and acknoledgments added image logo for ecocyc in the README file. deleted genome3 REP elements table. changed to version 0.51

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
     
 setup(name='RILseq',
-      version='0.50',
+      version='0.51',
       description='Processing RILSeq experiments results',
       long_description=readme(),
       classifiers=[
