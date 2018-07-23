9e8bff390e0c9162f4691e1ef392f894f46a242a
Asaf Peer <asafpr@gmail.com>
2018-07-23 06:42:17 +0300

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
     
 setup(name='RILseq',
-      version='0.59',
+      version='0.60',
       description='Processing RILSeq experiments results',
       long_description=readme(),
       classifiers=[
