97e601527c4dba861a290132e0163e54da6d8313
Niv Reiss <niv@lazuli.hanah>
2018-12-18 13:33:52 +0200

    changed version to 0.59

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
     
 setup(name='RILseq',
-      version='0.58',
+      version='0.59',
       description='Processing RILSeq experiments results',
       long_description=readme(),
       classifiers=[
