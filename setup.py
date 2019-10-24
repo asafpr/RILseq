a604ef4bec874482922bdde9a414225ebe11a9b5
Asaf Peer <Asaf.Peer@jax.org>
2019-10-24 10:57:27 -0400

    python3 comp

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
     
 setup(name='RILseq',
-      version='0.7.0',
+      version='0.70',
       description='Processing RILSeq experiments results',
       long_description=readme(),
       classifiers=[
