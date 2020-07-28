c9a8e5313243e7a162eb64ddc15a53ad94fdfaea
Yair Raz <yair.raz@mail.huji.ac.il>
2020-07-28 08:42:48 +0300

    Update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
     
 setup(name='RILseq',
-      version='0.77',
+      version='0.78',
       description='Processing RILSeq experiments results',
       long_description=readme(),
       classifiers=[
