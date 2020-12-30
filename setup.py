e3d2dfb4131303ea310a9e5d89be7397f2834060
Yair Raz <yair.raz@mail.huji.ac.il>
2020-12-30 20:45:44 +0200

    Updating the RILseq version to 0.81 after Amir's fix.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='RILseq',
-      version='0.80',
+      version='0.81',
       description='Processing RILSeq experiments results',
       long_description=readme(),
       classifiers=[
